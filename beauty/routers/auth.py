13c1f8ad9d7332ed7698e318c788b946f1a15314
long2ice <long2ice@gmail.com>
2022-12-14 22:21:39 +0800

    fix: login

--- beauty/routers/auth.py
+++ beauty/routers/auth.py
@@ -1,6 +1,7 @@
 import jwt
 from fastapi import APIRouter
 from pydantic import BaseModel
+from tortoise.exceptions import DoesNotExist
 
 from beauty import responses
 from beauty.models import User
@@ -17,6 +18,11 @@ class LoginBody(BaseModel):
 @router.post("/login", response_model=responses.LoginResponse)
 async def login(body: LoginBody):
     session = await code_to_session(body.code)
-    user, created = await User.get_or_create(openid=session.openid)
+    try:
+        user = await User.get(openid=session.openid)
+    except DoesNotExist:
+        user = await User.create(
+            openid=session.openid,
+        )
     token = jwt.encode({"user_id": user.pk}, settings.SECRET, algorithm=JWT_ALGORITHM)
     return responses.LoginResponse(token=token, user=user)
